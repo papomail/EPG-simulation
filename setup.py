cdbd667e722c226a3200bf84d2a20aff09df4429
papo <papomail@gmail.com>
2025-07-08 13:24:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12"
+        "Programming Language :: Python :: 3.12",
     ],
     python_requires=">=3.7",
     install_requires=[
